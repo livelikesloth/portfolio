aa72ddacfbb05e432d9dfb49ec31070f81348388
sloth <gkwkgo321@gmail.com>
2023-01-06 14:17:48 +0900

    1/5 포폴

--- src/card/MainCard.js
+++ src/card/MainCard.js
@@ -1,22 +1,29 @@
+import React, { useState } from 'react';
+import Logo from '../Logo';
+import Modal from './Modal';
 import'./MainCard.css';
+import Contact from '../Contact';
+
 
 function MainCard(){
-  return(
+  const [modalVisible, setModalVisible] = useState(false)
+  const openModal = () => {
+    setModalVisible(true)
+  }
+  const closeModal = () => {
+    setModalVisible(false)
+  }
+    return(
     <div className="cardFrame">
-      <div className="logo-container">
-        <div className="logo-box">
-          <p id="hyunj">HYUNJ</p> 
-          <p id="m">M</p>
-        </div>
-        <div className="logo-box" id="in">IN</div>
-        
-      </div>
+      <Logo/>
       <div className="item-container">
-        <p className="item"><a href="/">About</a></p>
+        <p className="item" onClick={openModal}><a href="/" onClick={event=>{ event.preventDefault();}}>ABOUT</a></p>
+        {modalVisible && <Modal visible={modalVisible} closable={true} maskClosable={true} onClose={closeModal}></Modal>}
         <p className="item"><a href="/">Portfolio</a></p>
-        <p className="item"><a href="/">Blog</a></p>
-        <p className="item"><a href="/">Github</a></p>
-        <p className="item"><a href="/">Instagram</a></p>               
+        <p className="item"><a href="https://livelikesloth.tistory.com/">BLOG</a></p>
+        <p className="item"><a href="https://github.com/livelikesloth">GITHUB</a></p>
+        <p className="item"><a href="https://instagram.com/livelikesloth_?igshid=YmMyMTA2M2Y=">INSTAGRAM</a></p>
+        <Contact></Contact>                             
       </div>
     </div>
   );
